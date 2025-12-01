99d35b8c0148014d273b7e5706d7316c94f82fdf
unknown <mrunalipatil9869@gmail.com>
2025-12-01 21:29:29 +0530

    Fix page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page Loaded Successfully</h1>
+      <p>Go to /dashboard to open your sales dashboard.</p>
+    </div>
+  );
+}
